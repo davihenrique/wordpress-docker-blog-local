f25361952ab7401aaffa39acc90840f06408013a
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-04-08 23:27:34 -0300

    refactor: Ajuste Porta

--- start.py
+++ start.py
@@ -1,8 +1,5 @@
 import webbrowser
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
-WORDPRESS_PORT = os.getenv('WORDPRESS_PORT')
 os.system('docker compose -f ".\docker-compose.yml" up -d --build')
-webbrowser.open('http://localhost:'+WORDPRESS_PORT)
+webbrowser.open('http://localhost:80')
